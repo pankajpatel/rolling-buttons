6a5282e000faa5f41e59582f1868cee00cab9c3e
Pankaj Patel <patel.pankaj@live.in>
2016-10-09 16:17:32 +0530

    enhanced functionality and added additional files

---
+++ README.md
@@ -0,0 +1,89 @@
+rolling-buttons jQuery plugin
+-------------------------
+This plugin creates the selected hyperlinks as rolling hyperlinks 
+
+```html
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src="jquery.rollingButtons.js"></script>
+<script type="text/javascript">
+  $(document).ready(function(e){
+    $('a').rollingButtons({ background: '#5F85B0', color: '#fff'});
+  });
+</script>
+<a href="time2hack.com">Time to Hack</a>
+```
+-----------------------------------------------------------------------------
+
+It does not require any explicit CSS or html. Though if you wanna add the css added by plugin to your regular CSS/LESS/SCSS files; you can add the following code block:
+
+```css
+.rollers {
+  display: inline-block;
+  overflow: hidden;
+  vertical-align: top;
+  -webkit-perspective: 600px;
+  -moz-perspective: 600px;
+  -ms-perspective: 600px;
+  perspective: 600px;
+  -webkit-perspective-origin: 50% 50%;
+  -moz-perspective-origin: 50% 50%;
+  -ms-perspective-origin: 50% 50%;
+  perspective-origin: 50% 50%;
+}
+.rollers:hover {
+  text-decoration:none;
+}
+.rollers span {
+  display: block;
+  position: relative;
+  padding: 0 2px;
+  -webkit-transition: all 400ms ease;
+  -moz-transition: all 400ms ease;
+  -ms-transition: all 400ms ease;
+  transition: all 400ms ease;
+  -webkit-transform-origin: 50% 0%;
+  -moz-transform-origin: 50% 0%;
+  -ms-transform-origin: 50% 0%;
+  transform-origin: 50% 0%;
+  -webkit-transform-style: preserve-3d;
+  -moz-transform-style: preserve-3d;
+  -ms-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+}
+.rollers:hover span {
+  background:'+ options.background +';
+  -webkit-transform: translate3d(0px, 0px, -30px) rotateX(90deg);
+  -moz-transform: translate3d(0px, 0px, -30px) rotateX(90deg);
+  -ms-transform: translate3d(0px, 0px, -30px) rotateX(90deg);
+  transform: translate3d(0px, 0px, -30px) rotateX(90deg);
+}
+.rollers span:after {
+  content: attr(data-title);
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  padding: 0 2px;
+  color:'+options.color+';
+  background:'+ options.background +';
+  -webkit-transform-origin: 50% 0%;
+  -moz-transform-origin: 50% 0%;
+  -ms-transform-origin: 50% 0%;
+  transform-origin: 50% 0%;
+  -webkit-transform: translate3d(0px, 105%, 0px) rotateX(-90deg);
+  -moz-transform: translate3d(0px, 105%, 0px) rotateX(-90deg);
+  -ms-transform: translate3d(0px, 105%, 0px) rotateX(-90deg);
+  transform: translate3d(0px, 105%, 0px) rotateX(-90deg);
+}
+```
+
+And if you have added above css in your CSS files, you can initiate the plugi as following code:
+
+```javascript
+$(document).ready(function(e){
+  $('a').rollingButtons({ background: '#5F85B0', color: '#fff', style: false, className: 'rollers' });
+});
+```
+
+Just try and play with it
+
--- jQuery.rollingButtons.js
+++ jQuery.rollingButtons.js
@@ -7,26 +7,27 @@
 */
 
 var options = {
-	background : '#DD4D42',
-	color : '#fff'
+  background : '#DD4D42',
+  color : '#fff',
+  className: 'rollers',
+  style: true
 };
-
+var style = '<style>.rollers {display: inline-block;overflow: hidden;vertical-align: top;-webkit-perspective: 600px;-moz-perspective: 600px;-ms-perspective: 600px;perspective: 600px;-webkit-perspective-origin: 50% 50%;-moz-perspective-origin: 50% 50%;-ms-perspective-origin: 50% 50%;perspective-origin: 50% 50%;}.rollers:hover {text-decoration:none;}.rollers span {display: block;position: relative;padding: 0 2px;-webkit-transition: all 400ms ease;-moz-transition: all 400ms ease;-ms-transition: all 400ms ease;transition: all 400ms ease;-webkit-transform-origin: 50% 0%;-moz-transform-origin: 50% 0%;-ms-transform-origin: 50% 0%;transform-origin: 50% 0%;-webkit-transform-style: preserve-3d;-moz-transform-style: preserve-3d;-ms-transform-style: preserve-3d;transform-style: preserve-3d;}.rollers:hover span {background: '+ options.background +';-webkit-transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );-moz-transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );-ms-transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );}.rollers span:after {content: attr(data-title);display: block;position: absolute;left: 0;top: 0;padding: 0 2px;color: '+options.color+';background: '+ options.background +';-webkit-transform-origin: 50% 0%;   -moz-transform-origin: 50% 0%;   -ms-transform-origin: 50% 0%;   transform-origin: 50% 0%;-webkit-transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );   -moz-transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );   -ms-transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );   transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );}</style>';
 (function($){
-	$.fn.rollingButtons = function(opts){
-		//Link background colors 
-		if(opts != null){
-			options = opts
-		}
-		var style = '<style>.rollers {display: inline-block;overflow: hidden;vertical-align: top;-webkit-perspective: 600px;-moz-perspective: 600px;-ms-perspective: 600px;perspective: 600px;-webkit-perspective-origin: 50% 50%;-moz-perspective-origin: 50% 50%;-ms-perspective-origin: 50% 50%;perspective-origin: 50% 50%;}.rollers:hover {text-decoration:none;}.rollers span {display: block;position: relative;padding: 0 2px;-webkit-transition: all 400ms ease;-moz-transition: all 400ms ease;-ms-transition: all 400ms ease;transition: all 400ms ease;-webkit-transform-origin: 50% 0%;-moz-transform-origin: 50% 0%;-ms-transform-origin: 50% 0%;transform-origin: 50% 0%;-webkit-transform-style: preserve-3d;-moz-transform-style: preserve-3d;-ms-transform-style: preserve-3d;transform-style: preserve-3d;}.rollers:hover span {background: '+ options.background +';-webkit-transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );-moz-transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );-ms-transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );transform: translate3d( 0px, 0px, -30px ) rotateX( 90deg );}.rollers span:after {content: attr(data-title);display: block;position: absolute;left: 0;top: 0;padding: 0 2px;color: '+options.color+';background: '+ options.background +';-webkit-transform-origin: 50% 0%;   -moz-transform-origin: 50% 0%;   -ms-transform-origin: 50% 0%;   transform-origin: 50% 0%;-webkit-transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );   -moz-transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );   -ms-transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );   transform: translate3d( 0px, 105%, 0px ) rotateX( -90deg );}</style>';
+  $.fn.rollingButtons = function(opts){
+    //Link background colors
+    $.extend(options, opts);
 
-		return this.each(function(i,obj){
-			$this = $(this);
-			//$anchors = $this;
-			$('body').prepend(style);
-			$.each($this, function(j,ele){
-				x= $(ele).html();
-				$(ele).addClass('rollers').html('<span data-title="'+x+'">'+x+'</span>');
-			});
-		});
-	};
+    return this.each(function(i,obj){
+      $this = $(this);
+      //$anchors = $this;
+      if(options.style == false ){
+        $('body').prepend(style);
+      }
+      $.each($this, function(j,ele){
+        var html = $(ele).html();
+        $(ele).addClass(options.className).html('<span data-title="'+html+'">'+html+'</span>');
+      });
+    });
+  };
 })(jQuery);
